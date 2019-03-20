962d4bd2f4778f369a49cd242d6ce38bbf3665c9
rabba alam <rabba.alam@oracle.com>
2019-03-20 08:49:50 +0530

    initial commit

---
+++ src/main/java/com/rabba007whizz/petclinic/RaalamPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.rabba007whizz.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaalamPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaalamPetClinicApplication.class, args);
+	}
+
+}
