3781add658f13f3714d2b4d79e78913adcc222b2
rabba alam <rabba.alam@oracle.com>
2019-03-23 10:58:52 +0530

    adds pojo models. Closes #2

---
+++ src/main/java/com/rabba007whizz/petclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package com.rabba007whizz.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+}
---
+++ src/main/java/com/rabba007whizz/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.rabba007whizz.petclinic.model;
+
+public class Vet extends Person {
+
+}
