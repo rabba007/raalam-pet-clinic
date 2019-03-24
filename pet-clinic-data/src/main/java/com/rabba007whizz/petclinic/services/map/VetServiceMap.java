9eed81165955c61feef55a8314a16472f750a4ce
rabba alam <rabba.alam@oracle.com>
2019-03-24 11:17:26 +0530

    implements Map based services, closes#6

---
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,39 @@
+package com.rabba007whizz.petclinic.services.map;
+
+import java.util.Set;
+
+import com.rabba007whizz.petclinic.model.Vet;
+import com.rabba007whizz.petclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
