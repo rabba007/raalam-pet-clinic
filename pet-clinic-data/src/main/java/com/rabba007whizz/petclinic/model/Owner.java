6577c1fa7214c68b756cb633a28348728be62c12
rabba alam <rabba.alam@oracle.com>
2019-03-26 18:59:54 +0530

    implements map service impl for PetType. Closes #27

--- pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/Owner.java
@@ -9,6 +9,31 @@ public class Owner extends Person {
     private String telephone;
     private Set<Pet> pets;
 
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
---
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.rabba007whizz.petclinic.services;
+
+import com.rabba007whizz.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
