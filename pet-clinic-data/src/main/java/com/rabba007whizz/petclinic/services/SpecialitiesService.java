175c316d75df9ef3ff5eae248042a3a6c14f3b0f
rabba alam <rabba.alam@oracle.com>
2019-03-27 05:55:19 +0530

    adds specilities map service impl. Closes #29

---
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.rabba007whizz.petclinic.services;
+
+import com.rabba007whizz.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
