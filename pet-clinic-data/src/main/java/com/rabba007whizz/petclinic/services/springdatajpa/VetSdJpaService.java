18ed10b3ec4a89b4af2e2a9f7d0990cf06cfec0d
rabba alam <rabba.alam@oracle.com>
2019-03-29 05:56:36 +0530

    adds spring data java service impl for Vet. Closes #28

---
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/services/springdatajpa/VetSdJpaService.java
@@ -0,0 +1,51 @@
+package com.rabba007whizz.petclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.rabba007whizz.petclinic.model.Vet;
+import com.rabba007whizz.petclinic.repositories.VetRepository;
+import com.rabba007whizz.petclinic.services.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetSdJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSdJpaService(VetRepository vetRepository) {
+        super();
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().iterator().forEachRemaining(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+
+}
