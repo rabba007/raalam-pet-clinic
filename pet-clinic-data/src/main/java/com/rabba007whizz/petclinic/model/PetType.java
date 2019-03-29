dd8b7cf10ad3505e713eb2a419fbbec090c8bc87
rabba alam <rabba.alam@oracle.com>
2019-03-29 09:24:12 +0530

    refactors the model for lombok support. Closes #45

--- pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/PetType.java
@@ -3,17 +3,22 @@ package com.rabba007whizz.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     private String name;
 
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/rabba007whizz/petclinic/model/Speciality.java
@@ -3,19 +3,22 @@ package com.rabba007whizz.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 
 }
